5ad1eb1dac1730166008939aa9f4f68cb7aad95e
WesC0511 <145807721+WesC0511@users.noreply.github.com>
2024-01-06 23:29:48 -0800

    Update api_keys.py

--- Starter_Code/starter_code/api_keys.py
+++ Starter_Code/starter_code/api_keys.py
@@ -1,5 +1,5 @@
 # OpenWeatherMap API Key
-weather_api_key = "5aac4147d3c819ebc1895c7978ab10b9"
+weather_api_key = "INPUT"
 
 # Geoapify API Key
-geoapify_key = "68ade16610be4cbdaa176c9ade5dc7e3"
+geoapify_key = "INPUT"
